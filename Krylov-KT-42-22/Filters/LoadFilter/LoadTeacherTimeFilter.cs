704579b81351086834ae994f9637f280acd89bf9
KINCHERUSKA <kryovv718@gmail.com>
2025-04-04 00:48:51 +0300

    backdown

--- Krylov-KT-42-22/Filters/LoadFilter/LoadTeacherTimeFilter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Krylov_KT_42_22.Filters.LoadFilter
-{
-    public class LoadTeacherTimeFilter
-    {
-        public int TeacherId { get; set; }
-        public int? DisciplineId { get; set; }
-        public int DepartmentId { get; set; }
-    }
-}
--- Krylov-KT-42-22/Filters/TeacherFilters/TeacherDepartmentFilter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Krylov_KT_42_22.Filters.TeacherFilters
-{
-    public class TeacherDepartmentFilter
-    {
-        public string Dep_Name { get; set; }
-        public string Pos_Name { get; set; }
-        public string Deg_Name { get; set; }
-    }
-}
