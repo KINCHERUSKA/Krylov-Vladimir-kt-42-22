c6da8d1758bee758512c17a2533dd8eb32014d5a
KINCHERUSKA <kryovv718@gmail.com>
2025-04-07 01:11:02 +0300

    finish lb4 and creat load feat

--- Krylov-KT-42-22/ServiceExtensions/ServiceExtensions.cs
+++ Krylov-KT-42-22/ServiceExtensions/ServiceExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Krylov_KT_42_22.Interfaces.DepartmentInterfaces.Krylov_KT_42_22.Interfaces.DepartmentInterfaces;
 using Krylov_KT_42_22.Interfaces.DepartmentInterfaces;
 using Krylov_KT_42_22.Interfaces.DisciplineInterfaces;
+using Krylov_KT_42_22.Interfaces.LoadInterfaces;
 using Krylov_KT_42_22.Interfaces.TeacherInterfaces;
 
 namespace Krylov_KT_42_22.ServiceExtensions
@@ -12,6 +13,7 @@ namespace Krylov_KT_42_22.ServiceExtensions
             services.AddScoped<ITeacherService, TeacherService>();
             services.AddScoped<IDepartmentService, DepartmentService>();
             services.AddScoped<IDisciplineService, DisciplineService>();
+            services.AddScoped<ILoadService, LoadService>();
             return services;
         }
     }
